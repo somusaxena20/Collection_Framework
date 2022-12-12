e94c3dfbc9bca6f78e53a57f5265305877683be7
somusaxena20 <somusaxena20@gmail.com>
2022-12-12 23:03:37 +0530

    first commit

---
+++ list/ArrayListExample.java
@@ -0,0 +1,26 @@
+package list;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Object> l = new ArrayList<>();
+
+        l.add("A");
+        l.add(10);
+        l.add("A");
+        l.add(null);
+
+        System.out.println(l);
+
+        l.add(2, "M");
+        
+        System.out.println(l);
+
+        l.remove(1);
+
+        System.out.println(l);
+    }
+}
